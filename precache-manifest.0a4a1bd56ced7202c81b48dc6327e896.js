ce83e667ce28bb7e046b6088465c3c60eb8a3d2e
Jakub Kuncicki <jakub.kuncicki@gmail.com>
2019-07-02 13:49:18 +0200

    Updates

---
+++ precache-manifest.0a4a1bd56ced7202c81b48dc6327e896.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6a1ef985723ea30a62fcf883073b97c",
+    "url": "/Oddam_w_dobre_rece/index.html"
+  },
+  {
+    "revision": "6e4628f5dd7ca3546bf9",
+    "url": "/Oddam_w_dobre_rece/static/css/main.9caf03d7.chunk.css"
+  },
+  {
+    "revision": "b4e915114ebe872f61e2",
+    "url": "/Oddam_w_dobre_rece/static/js/2.7667e28c.chunk.js"
+  },
+  {
+    "revision": "6e4628f5dd7ca3546bf9",
+    "url": "/Oddam_w_dobre_rece/static/js/main.6965ac6a.chunk.js"
+  },
+  {
+    "revision": "a973c81f9a3474e62695",
+    "url": "/Oddam_w_dobre_rece/static/js/runtime~main.dfd15aad.js"
+  },
+  {
+    "revision": "d184db2cc2f89dfaee3f499c80cfe751",
+    "url": "/Oddam_w_dobre_rece/static/media/Bg.d184db2c.jpg"
+  },
+  {
+    "revision": "0e48fc7f4f6fe85f5d1a34bdb30a84af",
+    "url": "/Oddam_w_dobre_rece/static/media/about.0e48fc7f.jpg"
+  },
+  {
+    "revision": "e5c092b715eebc3818968c2d47f4455e",
+    "url": "/Oddam_w_dobre_rece/static/media/bg.e5c092b7.jpg"
+  },
+  {
+    "revision": "74c241482c275f28baa081947a4616db",
+    "url": "/Oddam_w_dobre_rece/static/media/footer_bg_img.74c24148.jpg"
+  },
+  {
+    "revision": "85c98296eeecd76a9bf40cb6c5827b20",
+    "url": "/Oddam_w_dobre_rece/static/media/header_logged_img.85c98296.jpg"
+  },
+  {
+    "revision": "2c60c6fe9605a0b26170b834fdc6244f",
+    "url": "/Oddam_w_dobre_rece/static/media/header_start_img.2c60c6fe.jpg"
+  },
+  {
+    "revision": "1887b050053810c352e008bf8efb4f9e",
+    "url": "/Oddam_w_dobre_rece/static/media/merriweather-light.1887b050.woff2"
+  },
+  {
+    "revision": "8b8eb9223f2ca129740e6213550e9bf9",
+    "url": "/Oddam_w_dobre_rece/static/media/merriweather-light.8b8eb922.woff"
+  },
+  {
+    "revision": "3b5eccadb43ccd3a9a9cbe6ef5cbb7d9",
+    "url": "/Oddam_w_dobre_rece/static/media/merriweather-lightitalic.3b5eccad.woff2"
+  },
+  {
+    "revision": "8ea684b078a3fc0d05c15e03080bd111",
+    "url": "/Oddam_w_dobre_rece/static/media/merriweather-lightitalic.8ea684b0.woff"
+  },
+  {
+    "revision": "a92da91e71087f0a133a401f6ac03f57",
+    "url": "/Oddam_w_dobre_rece/static/media/merriweather-regular.a92da91e.woff"
+  },
+  {
+    "revision": "c1a9b1f0cc03776cc081aa629866f8e6",
+    "url": "/Oddam_w_dobre_rece/static/media/merriweather-regular.c1a9b1f0.woff2"
+  },
+  {
+    "revision": "115e728e23863bafa10516e0a4e9b34e",
+    "url": "/Oddam_w_dobre_rece/static/media/opensans-light.115e728e.woff2"
+  },
+  {
+    "revision": "9516d88d71c7997b398630c82f5dde80",
+    "url": "/Oddam_w_dobre_rece/static/media/opensans-light.9516d88d.woff"
+  },
+  {
+    "revision": "36c7dc5524370e3d005dbc97d9d7c7a2",
+    "url": "/Oddam_w_dobre_rece/static/media/opensans-regular.36c7dc55.woff"
+  },
+  {
+    "revision": "addd60afce57cbc949ca3cee188d4abc",
+    "url": "/Oddam_w_dobre_rece/static/media/opensans-regular.addd60af.woff2"
+  }
+]);
